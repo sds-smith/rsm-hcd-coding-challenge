4fea039731d69a18e60feb72ff33ca02ee6d696e
Shawn Smith <sds.smith24@gmail.com>
2022-12-21 13:18:37 -0500

    import and use cors for client origin on localhost

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const api = require('./routes/api');
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 app.use(express.json());
 
