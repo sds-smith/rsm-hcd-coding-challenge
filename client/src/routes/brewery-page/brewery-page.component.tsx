6b3fea4bbb1905626656894569d08c7cd99648ce
Shawn Smith <sds.smith24@gmail.com>
2022-12-23 12:36:13 -0500

    implement back btn in brewery page

--- client/src/routes/brewery-page/brewery-page.component.tsx
+++ client/src/routes/brewery-page/brewery-page.component.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import BreweryCard from '../../components/brewery-card/brewery-card.component';
 
+import './brewery-page.styles.scss';
+
 const BreweryPage = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1)
+    };
+
     return (
         <div>
+            <div className='backButton' onClick={goBack}>{'<-- Back'}</div>
             <BreweryCard />
         </div>
 
