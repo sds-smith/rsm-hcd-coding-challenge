3606fe1615183458cf3419dfee76f909e713e116
Shawn Smith <sds.smith24@gmail.com>
2022-12-24 08:20:43 -0500

    remove cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,9 @@
 const path = require('path');
 const express = require('express');
-const cors = require('cors');
 
 const api = require('./routes/api');
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
